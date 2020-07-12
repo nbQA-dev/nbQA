bafb3c82606e0b1c89021154d5b3996e6e09c739
Marco Gorelli <m.e.gorelli@gmail.com>
2020-07-12 10:59:46 +0100

    write black test in a way that passes

--- tests/test_black.py
+++ tests/test_black.py
@@ -17,25 +17,21 @@ def test_black_works(tmpdir):
         after = handle.readlines()
 
     result = "".join(difflib.unified_diff(before, after))
-    expected = """
-        ---
-        +++
-        @@ -48,7 +48,7 @@
-            "%%time\n",
-            "def hello(name: str = \"world\"):\n",
-            "\n",
-        -    "    return f'hello {name}'\n",
-        +    "    return f\"hello {name}\"\n",
-            "\n",
-            "\n",
-            "hello(3)"
 
-        """
+    expected = (
+        "--- \n"
+        "+++ \n"
+        "@@ -48,7 +48,7 @@\n"
+        '     "%%time\\n",\n'
+        '     "def hello(name: str = \\"world\\"):\\n",\n'
+        '     "\\n",\n-    "    return f\'hello {name}\'\\n",\n'
+        '+    "    return f\\"hello {name}\\"\\n",\n     "\\n",\n'
+        '     "\\n",\n     "hello(3)"\n'
+    )
     (Path("tests/data") / "test_notebook.ipynb").unlink()
 
     shutil.copy(
         str(Path(tmpdir) / "test_notebook.ipynb"),
         str(Path("tests/data") / "test_notebook.ipynb"),
     )
-    breakpoint()
     assert result == expected
