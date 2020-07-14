de40e793dc60ebf8e42ea34d2e2fe26b3a29fe06
Marco Gorelli <m.e.gorelli@gmail.com>
2020-07-14 08:37:36 +0100

    own parser

--- nbqa/replace_source.py
+++ nbqa/replace_source.py
@@ -1,24 +1,31 @@
 import json
-import subprocess
 
 
 def main(python_file, notebook):
     with open(notebook, "r") as handle:
-        nb = handle.read()
+        notebook_json = json.load(handle)
 
-    subprocess.run(["ipynb-py-convert", python_file, notebook])
+    with open(python_file, "r") as handle:
+        pyfile = handle.read()
 
-    with open(notebook, "r") as handle:
-        new_sources = json.loads(handle.read())["cells"]
-    notebook_json = json.loads(nb)
-    new_cells = [
+    pycells = pyfile[len("# %%") :].split("\n\n\n# %%")
+
+    new_sources = []
+    for i in pycells:
+        cells = i.splitlines(True)
+        if cells[0] == "\n":
+            new_sources.append({"source": cells[1:], "cell_type": "code"})
+        else:
+            new_sources.append({"source": cells[1:], "cell_type": "markdown"})
+
+    new_sources = [
         {
             **{key: val for key, val in i.items() if i != "source"},
             **{"source": new_sources[n]["source"]},
         }
         for n, i in enumerate(notebook_json["cells"])
     ]
-    notebook_json.update({"cells": new_cells})
+    notebook_json.update({"cells": new_sources})
     with open(notebook, "w") as handle:
         json.dump(notebook_json, handle, indent=1, ensure_ascii=False)
         handle.write("\n")
--- tests/test_black.py
+++ tests/test_black.py
@@ -1,19 +1,19 @@
 import difflib
-import subprocess
+
+import pytest
+
+from nbqa.__main__ import main
 
 
-def test_black_works(tmp_notebook_for_testing):
+def test_black_works(tmp_notebook_for_testing, capsys):
     """
     Check black works. Should only reformat code cells.
     """
     # check diff
     with open(tmp_notebook_for_testing, "r") as handle:
         before = handle.readlines()
-    output = subprocess.run(
-        ["python", "-m", "nbqa", "black", "tests/data/notebook_for_testing.ipynb"],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-    )
+    with pytest.raises(SystemExit):
+        main(["black", "tests/data/notebook_for_testing.ipynb"])
     with open(tmp_notebook_for_testing, "r") as handle:
         after = handle.readlines()
     result = "".join(difflib.unified_diff(before, after))
@@ -30,9 +30,10 @@ def test_black_works(tmp_notebook_for_testing):
     assert result == expected
 
     # check out and err
+    out, err = capsys.readouterr()
     expected_out = ""
     expected_err = (
         "reformatted notebook_for_testing.ipynb\nAll done! ✨ 🍰 ✨\n1 file reformatted.\n"
     )
-    assert output.stdout.decode() == expected_out
-    assert output.stderr.decode() == expected_err
+    assert out == expected_out
+    assert err == expected_err
