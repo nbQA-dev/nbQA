222aa1e1eef2d6b273d45ffabe50f7019c785b33
Marco Gorelli <m.e.gorelli@gmail.com>
2020-07-16 08:20:39 +0100

    clean up

--- nbqa/replace_source.py
+++ nbqa/replace_source.py
@@ -1,6 +1,13 @@
 import json
 
 
+def _parse_python_cell(cell):
+    source = cell.splitlines(True)
+    if source[0] == "\n":
+        return {"source": source[1:], "cell_type": "code"}
+    return {"source": source[1:], "cell_type": "markdown"}
+
+
 def main(python_file, notebook):
     with open(notebook, "r") as handle:
         notebook_json = json.load(handle)
@@ -10,22 +17,16 @@ def main(python_file, notebook):
 
     pycells = pyfile[len("# %%") :].split("\n\n\n# %%")
 
-    new_sources = []
-    for i in pycells:
-        cells = i.splitlines(True)
-        if cells[0] == "\n":
-            new_sources.append({"source": cells[1:], "cell_type": "code"})
-        else:
-            new_sources.append({"source": cells[1:], "cell_type": "markdown"})
+    new_sources = (_parse_python_cell(i) for i in pycells)
 
-    new_sources = [
+    new_cells = [
         {
             **{key: val for key, val in i.items() if i != "source"},
-            **{"source": new_sources[n]["source"]},
+            **{"source": j["source"]},
         }
-        for n, i in enumerate(notebook_json["cells"])
+        for n, (i, j) in enumerate(zip(notebook_json["cells"], new_sources))
     ]
-    notebook_json.update({"cells": new_sources})
+    notebook_json.update({"cells": new_cells})
     with open(notebook, "w") as handle:
         json.dump(notebook_json, handle, indent=1, ensure_ascii=False)
         handle.write("\n")
