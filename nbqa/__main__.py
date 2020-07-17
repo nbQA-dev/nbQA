411be2d3d4bbb1e8d699a48ac14638a283e55f89
Marco Gorelli <m.e.gorelli@gmail.com>
2020-07-17 11:19:58 +0100

    remove temp comment

--- nbqa/__main__.py
+++ nbqa/__main__.py
@@ -81,7 +81,6 @@ def _replace_temp_python_file_references_in_out_err(
         else:
             cell_count += 1
         mapping[n + 1] = f"cell_{cell_no}:{cell_count}"
-    # this is easy - all you need to do is map the cell line as well.
     out = re.sub(
         rf"(?<={notebook.name}:)\d+", lambda x: str(mapping[int(x.group())]), out,
     )
