26321923af9e1a097f77b5f6ab3889855b01091b
Marco Gorelli <m.e.gorelli@gmail.com>
2020-07-19 15:43:12 +0100

    linesep

--- tests/test_black.py
+++ tests/test_black.py
@@ -30,6 +30,6 @@ def test_black_works(tmp_notebook_for_testing, capsys):
     # check out and err
     out, err = capsys.readouterr()
     expected_out = ""
-    expected_err = f"reformatted {path}\nAll done! ✨ 🍰 ✨\n1 file reformatted.\n"  # noqa
+    expected_err = f"reformatted {path}{os.linesep}All done! ✨ 🍰 ✨{os.linesep}1 file reformatted.{os.linesep}"  # noqa
     assert out == expected_out
     assert err == expected_err
