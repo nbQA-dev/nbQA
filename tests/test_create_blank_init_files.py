1e01a48ec2f739f8ab4ef4582ab5c18ceb45cc65
Marco Gorelli <m.e.gorelli@gmail.com>
2020-07-20 17:07:45 +0100

    use existing notebook in test init

--- tests/test_create_blank_init_files.py
+++ tests/test_create_blank_init_files.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from nbqa.__main__ import _create_blank_init_files
@@ -7,6 +8,12 @@ def test_create_blank_init_files(tmpdir):
     """
     Check that if a notebook is in current working directory then no init file is made.
     """
-    _create_blank_init_files(Path("some_notebook.ipynb"), tmpdir)
+    _create_blank_init_files(
+        Path(os.path.join("tests", "data", "notebook_for_testing.ipynb")), tmpdir
+    )
     result = list(Path(tmpdir).rglob("__init__.py"))
-    assert result == []
+    expected = [
+        Path(tmpdir).joinpath(os.path.join("tests", "__init__.py")),
+        Path(tmpdir).joinpath(os.path.join("tests", "data", "__init__.py")),
+    ]
+    assert result == expected
