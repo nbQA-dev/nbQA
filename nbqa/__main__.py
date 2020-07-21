890a94e3258fc64587c5f65160a83a3009ffff1d
Marco Gorelli <m.e.gorelli@gmail.com>
2020-07-21 07:13:39 +0100

    let's try both

--- nbqa/__main__.py
+++ nbqa/__main__.py
@@ -72,8 +72,14 @@ def _replace_full_path_out_err(out, err, temp_python_file, notebook):
     """
     Take care of case when out/err display full path.
     """
-    out = out.replace(str(Path(temp_python_file).resolve()), str(notebook.resolve()))
-    err = err.replace(str(Path(temp_python_file).resolve()), str(notebook.resolve()))
+    out = out.replace(str(temp_python_file), str(notebook.resolve()))
+    err = err.replace(str(temp_python_file), str(notebook.resolve()))
+
+    # This next part is necessary to handle cases when `resolve` changes the path.
+    # I couldn't reproduce this locally, but during CI, on the Windows job, I found
+    # that VSSADM~1 was changing into VssAdministrator.
+    out = out.replace(str(temp_python_file.resolve()), str(notebook.resolve()))
+    err = err.replace(str(temp_python_file.resolve()), str(notebook.resolve()))
     return out, err
 
 
