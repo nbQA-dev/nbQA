f2960072541a9d1723f25779e2cbe45d91d511ab
Marco Gorelli <m.e.gorelli@gmail.com>
2020-08-02 13:13:26 +0100

    use regex

--- tests/test_allow_mutation.py
+++ tests/test_allow_mutation.py
@@ -10,22 +10,22 @@ from nbqa.__main__ import main
 def test_allow_mutation() -> None:
     """Check black, without --nbqa-mutate, errors."""
     path = os.path.abspath(os.path.join("tests", "data", "notebook_for_testing.ipynb"))
-    msg = fr"nbqa black {path} --nbqa-mutate"
+    msg = fr".*nbqa black {path} --nbqa-mutate.*"
     with pytest.raises(
         SystemExit, match=msg,
     ):
         main(["black", path])
-    msg = fr"nbqa black {path} --line-length 96 --nbqa-mutate"
+    msg = fr".*nbqa black {path} --line-length 96 --nbqa-mutate.*"
     with pytest.raises(
         SystemExit, match=msg,
     ):
         main(["black", path, "--line-length", "96"])
-    msg = fr"nbqa black {path} --nbqa-config=setup.cfg --nbqa-mutate"
+    msg = fr".*nbqa black {path} --nbqa-config=setup.cfg --nbqa-mutate.*"
     with pytest.raises(
         SystemExit, match=msg,
     ):
         main(["black", path, "--nbqa-config=setup.cfg"])
-    msg = fr"nbqa black {path} --nbqa-preserve-init --nbqa-mutate"
+    msg = fr".*nbqa black {path} --nbqa-preserve-init --nbqa-mutate.*"
     with pytest.raises(
         SystemExit, match=msg,
     ):
