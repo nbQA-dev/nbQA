ef159c03bd30b5ed7d715cf207dccc354091a55c
Marco Gorelli <m.e.gorelli@gmail.com>
2020-08-16 14:45:26 +0100

    use local installation

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,5 +5,5 @@ import subprocess
 
 def test_version() -> None:
     """Check you can run :code:`nbqa --version`."""
-    output = subprocess.run(["python", "-m", "nbqa", "--version"])
+    output = subprocess.run(["nbqa", "--version"])
     assert output.returncode == 0
