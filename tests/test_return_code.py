b20a59998dceb21d698f8e1741e54d09e01236ff
Marco Gorelli <m.e.gorelli@gmail.com>
2020-08-16 14:48:33 +0100

    use local installation (#165)

--- tests/test_return_code.py
+++ tests/test_return_code.py
@@ -9,12 +9,12 @@ CLEAN_NOTEBOOK = os.path.join("tests", "data", "clean_notebook.ipynb")
 
 def test_flake8_return_code() -> None:
     """Check flake8 returns 0 if it passes, 1 otherwise."""
-    output = subprocess.run(["python", "-m", "nbqa", "flake8", DIRTY_NOTEBOOK])
+    output = subprocess.run(["nbqa", "flake8", DIRTY_NOTEBOOK])
     result = output.returncode
     expected = 1
     assert result == expected
 
-    output = subprocess.run(["python", "-m", "nbqa", "flake8", CLEAN_NOTEBOOK])
+    output = subprocess.run(["nbqa", "flake8", CLEAN_NOTEBOOK])
     result = output.returncode
     expected = 0
     assert result == expected
@@ -22,28 +22,22 @@ def test_flake8_return_code() -> None:
 
 def test_black_return_code() -> None:
     """Check black returns 0 if it passes, 1 otherwise."""
-    output = subprocess.run(
-        ["python", "-m", "nbqa", "black", DIRTY_NOTEBOOK, "--check"]
-    )
+    output = subprocess.run(["nbqa", "black", DIRTY_NOTEBOOK, "--check"])
     result = output.returncode
     expected = 1
     assert result == expected
 
-    output = subprocess.run(
-        ["python", "-m", "nbqa", "black", "--check", CLEAN_NOTEBOOK]
-    )
+    output = subprocess.run(["nbqa", "black", "--check", CLEAN_NOTEBOOK])
     result = output.returncode
     expected = 0
     assert result == expected
 
-    output = subprocess.run(["python", "-m", "nbqa", "black", "--check", "tests"])
+    output = subprocess.run(["nbqa", "black", "--check", "tests"])
     result = output.returncode
     expected = 1
     assert result == expected
 
-    output = subprocess.run(
-        ["python", "-m", "nbqa", "black", CLEAN_NOTEBOOK, "--check", "-l", "1"]
-    )
+    output = subprocess.run(["nbqa", "black", CLEAN_NOTEBOOK, "--check", "-l", "1"])
     result = output.returncode
     expected = 1
     assert result == expected
