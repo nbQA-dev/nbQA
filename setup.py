5b522d8ba0fcc57d97083ffb2ce2eefe4a97f56a
Marco Gorelli <m.e.gorelli@gmail.com>
2020-09-04 10:28:42 +0100

    v0.1.27 (#188)

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/MarcoGorelli/nbQA",
-    version="0.1.26",
+    version="0.1.27",
     zip_safe=False,
 )
