3b316a4ffade4a16ab0fab7d547c9e36778de474
Marco Gorelli <m.e.gorelli@gmail.com>
2020-10-03 13:18:37 +0100

    :memo:

--- tests/test_unable_to_reconstruct.py
+++ tests/test_unable_to_reconstruct.py
@@ -8,16 +8,7 @@ from nbqa.__main__ import main
 
 
 def test_unable_to_reconstruct_message() -> None:
-    """
-    Check black works. Should only reformat code cells.
-
-    Parameters
-    ----------
-    tmp_notebook_for_testing
-        Temporary copy of :code:`notebook_for_testing.ipynb`.
-    capsys
-        Pytest fixture to capture stdout and stderr.
-    """
+    """Check error message shows if we're unable to reconstruct notebook."""
     path = os.path.abspath(os.path.join("tests", "data", "notebook_for_testing.ipynb"))
 
     message = "Please report a bug at https://github.com/nbQA-dev/nbQA/issues"
