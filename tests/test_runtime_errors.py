b2860eb14d5889f483ecc1e7e53577eb730409ea
Marco Gorelli <m.e.gorelli@gmail.com>
2020-10-04 10:54:47 +0100

    .*

--- tests/test_runtime_errors.py
+++ tests/test_runtime_errors.py
@@ -11,7 +11,7 @@ from nbqa.__main__ import main
 def test_unable_to_reconstruct_message() -> None:
     """Check error message shows if we're unable to reconstruct notebook."""
     path = os.path.abspath(os.path.join("tests", "data", "notebook_for_testing.ipynb"))
-    message = fr"Error reconstructing {path}"
+    message = fr".*Error reconstructing {path}.*"
     with pytest.raises(RuntimeError, match=message):
         main(["remove_comments", path, "--nbqa-mutate"])
 
