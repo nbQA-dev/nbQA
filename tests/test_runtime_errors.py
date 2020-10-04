169fa024560eedad6409b174090d3a42be1aa10e
Marco Gorelli <m.e.gorelli@gmail.com>
2020-10-04 10:50:26 +0100

    escape

--- tests/test_runtime_errors.py
+++ tests/test_runtime_errors.py
@@ -11,7 +11,7 @@ from nbqa.__main__ import main
 def test_unable_to_reconstruct_message() -> None:
     """Check error message shows if we're unable to reconstruct notebook."""
     path = os.path.abspath(os.path.join("tests", "data", "notebook_for_testing.ipynb"))
-    message = f"Error reconstructing {path}"
+    message = fr"Error reconstructing {path}"
     with pytest.raises(RuntimeError, match=message):
         main(["remove_comments", path, "--nbqa-mutate"])
 
@@ -20,7 +20,7 @@ def test_unable_to_parse() -> None:
     """Check error message shows if we're unable to parse notebook."""
     path = Path("tests") / "data/invalid_notebook.ipynb"
     path.write_text("foo")
-    message = f"Error parsing {str(path)}"
+    message = fr"Error parsing {str(path)}"
     with pytest.raises(RuntimeError, match=message):
         main(["flake8", str(path), "--nbqa-mutate"])
     path.unlink()
