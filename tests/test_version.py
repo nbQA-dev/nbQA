fcda46279a22647d6b006254ff40a1b814e580cb
girish pasupathy <girishpasupathy@gmail.com>
2020-10-13 19:24:07 +0530

    Add more checks to the test_version test case
    Changes to be committed:
    	modified:   tests/test_version.py

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,9 +1,13 @@
 """Check you can run :code:`nbqa --version`."""
 
 import subprocess
+from subprocess import PIPE
+
+from nbqa import __version__
 
 
 def test_version() -> None:
     """Check you can run :code:`nbqa --version`."""
-    output = subprocess.run(["nbqa", "--version"])
+    output = subprocess.run(["nbqa", "--version"], stdout=PIPE, stderr=PIPE, text=True)
+    assert output.stdout.strip() == f"nbqa {__version__}"
     assert output.returncode == 0
