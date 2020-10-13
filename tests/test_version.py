74ba54942d84dc790e19c84690c8b08d0d303412
girish pasupathy <girishpasupathy@gmail.com>
2020-10-13 21:58:48 +0530

    Fixed tox failure with python 3.6 by replacing `text` with `universal_newlines` arg to `subprocess.run`
    Changes to be committed:
    	modified:   tests/test_version.py

--- tests/test_version.py
+++ tests/test_version.py
@@ -8,6 +8,8 @@ from nbqa import __version__
 
 def test_version() -> None:
     """Check you can run :code:`nbqa --version`."""
-    output = subprocess.run(["nbqa", "--version"], stdout=PIPE, stderr=PIPE, text=True)
+    output = subprocess.run(
+        ["nbqa", "--version"], stdout=PIPE, stderr=PIPE, universal_newlines=True
+    )
     assert output.stdout.strip() == f"nbqa {__version__}"
     assert output.returncode == 0
