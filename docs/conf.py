ef951302c3190312366dc2ff41d4d45445307ca3
s-weigand <s.weigand.phy@gmail.com>
2020-10-29 20:28:05 +0100

    Removed obsolete path injection, since nbqa is installed anyway
    This is the case for local development and RTD

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,8 @@
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
 #
-import os
-import sys
 
-sys.path.insert(0, os.path.abspath(".."))
-
-import nbqa  # noqa
+import nbqa
 
 # -- General configuration ---------------------------------------------
 
