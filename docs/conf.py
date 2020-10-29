7f2639dc19131b3db25aad81c876ac3c31517b01
s-weigand <s.weigand.phy@gmail.com>
2020-10-29 20:26:07 +0100

    Set sidebar nesting level to infinite

--- docs/conf.py
+++ docs/conf.py
@@ -97,6 +97,9 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "navigation_depth": -1,
+}
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
