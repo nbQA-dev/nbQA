aef7d2e7e0a871a5771a9237e3aad44b4b526116
Marco Gorelli <m.e.gorelli@gmail.com>
2020-11-05 18:34:39 +0000

    better test

--- tests/test_get_notebooks.py
+++ tests/test_get_notebooks.py
@@ -1,27 +1,28 @@
 """Check function which lists notebooks in directory."""
 
+import shutil
 from pathlib import Path
 from typing import TYPE_CHECKING
 
 from nbqa.__main__ import _get_notebooks
 
 if TYPE_CHECKING:
-    from _pytest.monkeypatch import MonkeyPatch
+    from py._path.local import LocalPath
+
+CLEAN_NOTEBOOK = Path("tests") / "data/clean_notebook.ipynb"
 
 
-def test_get_notebooks(monkeypatch: "MonkeyPatch"):
+def test_get_notebooks(tmpdir: "LocalPath"):
     """
     Check that unwanted directories are excluded.
 
     Parameters
     ----------
-    monkeypatch
-        Pytest fixture, we use it to override ``PYTHONPATH``.
+    tmpdir
+        Pytest fixture, gives us a temporary directory.
 
     """
-    result = list(_get_notebooks("tests"))
-    assert Path("tests") / "data/clean_notebook.ipynb" in result
-
-    monkeypatch.setattr("nbqa.__main__.EXCLUDES", r"/(data)/")
-    result = list(_get_notebooks("tests"))
+    Path(tmpdir / ".git/tests/data").mkdir(parents=True)
+    shutil.copy(str(CLEAN_NOTEBOOK), str(tmpdir / ".git" / CLEAN_NOTEBOOK))
+    result = list(_get_notebooks(tmpdir))
     assert not result
