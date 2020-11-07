a2870180ccc71e513d2b1a9b34ea11bcca851c55
Marco Gorelli <m.e.gorelli@gmail.com>
2020-11-07 18:41:50 +0000

    :doc:, :label:

--- tests/test_include_exclude.py
+++ tests/test_include_exclude.py
@@ -3,13 +3,25 @@
 import re
 from pathlib import Path
 from textwrap import dedent
+from typing import TYPE_CHECKING
 
 import pytest
 
 from nbqa.__main__ import main
 
+if TYPE_CHECKING:
+    from _pytest.capture import CaptureFixture
 
-def test_cli_files(capsys):
+
+def test_cli_files(capsys: "CaptureFixture"):
+    """
+    Test --nbqa-files is picked up correctly.
+
+    Parameters
+    ----------
+    capsys
+        Pytest fixture to capture stdout and stderr.
+    """
     with pytest.raises(SystemExit):
         main(["flake8", "tests", "--nbqa-files", "^tests/data/notebook_for"])
 
@@ -19,7 +31,15 @@ def test_cli_files(capsys):
     )
 
 
-def test_cli_exclude(capsys):
+def test_cli_exclude(capsys: "CaptureFixture"):
+    """
+    Test --nbqa-exclude is picked up correctly.
+
+    Parameters
+    ----------
+    capsys
+        Pytest fixture to capture stdout and stderr.
+    """
     with pytest.raises(SystemExit):
         main(["flake8", "tests", "--nbqa-exclude", "^tests/data/notebook_for"])
 
@@ -29,8 +49,15 @@ def test_cli_exclude(capsys):
     )
 
 
-def test_config_files(capsys):
+def test_config_files(capsys: "CaptureFixture"):
+    """
+    Test [nbqa.files] config is picked up correctly.
 
+    Parameters
+    ----------
+    capsys
+        Pytest fixture to capture stdout and stderr.
+    """
     Path("setup.cfg").write_text(
         dedent(
             """\
@@ -50,8 +77,15 @@ def test_config_files(capsys):
     )
 
 
-def test_config_exclude(capsys):
+def test_config_exclude(capsys: "CaptureFixture"):
+    """
+    Test [nbqa.exclude] config is picked up correctly.
 
+    Parameters
+    ----------
+    capsys
+        Pytest fixture to capture stdout and stderr.
+    """
     Path("setup.cfg").write_text(
         dedent(
             """\
