d266e18092e57da2022e205b73af60a1fc57078a
Sourcery AI <>
2020-11-12 14:47:32 +0000

    'Refactored by Sourcery'

--- nbqa/__main__.py
+++ nbqa/__main__.py
@@ -337,10 +337,9 @@ def _get_arg(
     'tmpdir/my_notebook.py'
     """
     if Path(root_dir).is_dir():
-        arg = Path(tmpdirname) / Path(root_dir).resolve().relative_to(project_root)
+        return Path(tmpdirname) / Path(root_dir).resolve().relative_to(project_root)
     else:
-        arg = nb_to_py_mapping[Path(root_dir)]
-    return arg
+        return nb_to_py_mapping[Path(root_dir)]
 
 
 def _get_all_args(
