2189fbfe64c7d4ff52f5ca80a69f034e035a9118
s-weigand <s.weigand.phy@gmail.com>
2020-11-22 17:06:19 +0100

    Removed sys.path patching

--- tests/test_runtime_errors.py
+++ tests/test_runtime_errors.py
@@ -2,7 +2,6 @@
 
 import os
 import re
-import sys
 from pathlib import Path
 from textwrap import dedent
 from typing import TYPE_CHECKING
@@ -95,7 +94,6 @@ def test_unable_to_reconstruct_message_pythonpath(monkeypatch: "MonkeyPatch") ->
     path = os.path.abspath(os.path.join("tests", "data", "notebook_for_testing.ipynb"))
     message = re.escape(f"Error reconstructing {path}")
     monkeypatch.setenv("PYTHONPATH", os.path.join(os.getcwd(), "tests"))
-    monkeypatch.setattr("sys.path", sys.path + [os.path.join(os.getcwd(), "tests")])
     with pytest.raises(RuntimeError, match=message):
         main(["remove_comments", path, "--nbqa-mutate"])
 
