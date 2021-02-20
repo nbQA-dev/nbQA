d4f1093befbea41a61e46e9326d33cd396010f07
Marco Gorelli <marcogorelli@protonmail.com>
2021-02-20 09:35:13 +0000

    set mypypath as well as pythonpath (#545)

--- tests/tools/test_mypy_works.py
+++ tests/tools/test_mypy_works.py
@@ -45,9 +45,32 @@ def test_mypy_works(capsys: "CaptureFixture") -> None:
         {path_0}:cell_2:19: error: Argument 1 to "hello" has incompatible type "int"; expected "str"
         {path_3}:cell_8:3: error: Name 'flake8_version' is not defined
         {path_3}:cell_8:4: error: Name 'flake8_version' is not defined
-        Found 5 errors in 4 files (checked 19 source files)
+        Found 5 errors in 4 files (checked 20 source files)
         """  # noqa
     )
     expected_err = ""
     assert sorted(out.splitlines()) == sorted(expected_out.splitlines())
     assert sorted(err.splitlines()) == sorted(expected_err.splitlines())
+
+
+def test_mypy_with_local_import(capsys: "CaptureFixture") -> None:
+    """
+    Check mypy can find local import.
+
+    Parameters
+    ----------
+    capsys
+        Pytest fixture to capture stdout and stderr
+    """
+    with pytest.raises(SystemExit):
+        main(
+            [
+                "mypy",
+                str(Path("tests") / "data/notebook_with_local_import.ipynb"),
+            ]
+        )
+
+    # check out and err
+    out, _ = capsys.readouterr()
+    expected = "Success: no issues found in 1 source file\n"
+    assert out == expected
