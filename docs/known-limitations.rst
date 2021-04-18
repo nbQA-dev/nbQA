1c57a520a3d07add390168c581894947f9dff684
Marco Gorelli <marcogorelli@protonmail.com>
2021-04-18 18:56:50 +0100

    preserve trailing semicolon if followed by comment (#571)
    * preserve trailing semicolon if followed by comment
    
    * add test data
    
    * Update tests/tools/test_black.py
    
    * Update tests/tools/test_black.py
    
    * Update tests/tools/test_isort_works.py
    
    * replace regex

--- docs/known-limitations.rst
+++ docs/known-limitations.rst
@@ -8,37 +8,6 @@ Automagic
 `Automagic <https://ipython.readthedocs.io/en/stable/interactive/magics.html?highlight=automagic#magic-automagic>`_ ("Make magic functions callable without having to type the initial %") will not work well with most code quality tools,
 as it will not parse as valid Python syntax.
 
-Black
------
-
-Comment after trailing semicolon
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-Putting a comment after a trailing semicolon will make ``black`` move the comment to the
-next line, and the semicolon will be lost.
-
-Example:
-
-.. code:: python
-
-    plt.plot();  # some comment
-
-
-Will be transformed to:
-
-.. code:: python
-
-    plt.plot()
-    # some comment
-
-You can overcome this limitation by moving the comment to the previous line - like this,
-the trailing semicolon will be preserved:
-
-.. code:: python
-
-    # some comment
-    plt.plot();
-
 Linters (flake8, mypy, pylint, ...)
 -----------------------------------
 
--- tests/tools/test_black.py
+++ tests/tools/test_black.py
@@ -500,3 +500,47 @@ def test_invalid_syntax_with_nbqa_diff(capsys: "CaptureFixture") -> None:
 
     assert expected_out == out
     assert expected_err == err
+
+
+def test_comment_after_trailing_comma(capsys: "CaptureFixture") -> None:
+    """
+    Check trailing semicolon is still preserved if comment is after it.
+
+    Parameters
+    ----------
+    capsys
+        Pytest fixture to capture stdout and stderr.
+    """
+    path = os.path.abspath(
+        os.path.join("tests", "data", "comment_after_trailing_semicolon.ipynb")
+    )
+
+    with pytest.raises(SystemExit):
+        main(["black", path, "--nbqa-diff"])
+
+    out, _ = capsys.readouterr()
+    expected_out = (
+        "\x1b[1mCell 1\x1b[0m\n"
+        "------\n"
+        f"--- {path}\n"
+        f"+++ {path}\n"
+        "@@ -1,4 +1,5 @@\n"
+        "\x1b[31m-import glob;\n"
+        "\x1b[0m\x1b[32m+import glob\n"
+        "\x1b[0m \n"
+        " import nbqa;\n"
+        "\x1b[32m+\n"
+        "\x1b[0m # this is a comment\n"
+        "\n"
+        "\x1b[1mCell 2\x1b[0m\n"
+        "------\n"
+        f"--- {path}\n"
+        f"+++ {path}\n"
+        "@@ -1,3 +1,2 @@\n"
+        " def func(a, b):\n"
+        "     pass;\n"
+        "\x1b[31m- \n"
+        "\x1b[0m\n"
+        "To apply these changes use `--nbqa-mutate` instead of `--nbqa-diff`\n"
+    )
+    assert out == expected_out
