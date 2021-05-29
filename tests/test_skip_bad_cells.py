af374fe64e172c1d7ba859777f19a3d9272e7bb1
Marco Gorelli <marcogorelli@protonmail.com>
2021-05-29 21:05:39 +0100

    add new test

---
+++ tests/test_skip_bad_cells.py
@@ -0,0 +1,27 @@
+"""Test the skip bad cells flag."""
+import os
+from typing import TYPE_CHECKING
+
+from nbqa.__main__ import main
+
+if TYPE_CHECKING:
+    from _pytest.capture import CaptureFixture
+
+
+def test_diff_present(capsys: "CaptureFixture") -> None:
+    """Test the results on --nbqa-diff on a dirty notebook."""
+    file = os.path.join("tests", "invalid_data", "automagic.ipynb")
+    main(["black", file, "--nbqa-diff", "--nbqa-skip-bad-cells"])
+    out, _ = capsys.readouterr()
+    expected_out = (
+        "\x1b[1mCell 1\x1b[0m\n"
+        "------\n"
+        f"--- {file}\n"
+        f"+++ {file}\n"
+        "@@ -1,2 +1,2 @@\n"
+        " if True:\n"
+        "\x1b[31m-    print('definitely valid')\n"
+        '\x1b[0m\x1b[32m+    print("definitely valid")\n'
+        "\x1b[0m\nTo apply these changes use `--nbqa-mutate` instead of `--nbqa-diff`\n"
+    )
+    assert out == expected_out
