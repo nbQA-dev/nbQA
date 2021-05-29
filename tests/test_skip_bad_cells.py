afe0140a8a7de7bc6e44424d915a54854992e6e1
Marco Gorelli <marcogorelli@protonmail.com>
2021-05-29 21:20:27 +0100

    extra test

--- tests/test_skip_bad_cells.py
+++ tests/test_skip_bad_cells.py
@@ -9,7 +9,7 @@ if TYPE_CHECKING:
 
 
 def test_cmdline(capsys: "CaptureFixture") -> None:
-    """Test the results on --nbqa-diff on a dirty notebook."""
+    """Check running from command-line."""
     file = os.path.join("tests", "invalid_data", "automagic.ipynb")
     main(["black", file, "--nbqa-diff", "--nbqa-skip-bad-cells"])
     out, _ = capsys.readouterr()
@@ -25,3 +25,27 @@ def test_cmdline(capsys: "CaptureFixture") -> None:
         "\x1b[0m\nTo apply these changes use `--nbqa-mutate` instead of `--nbqa-diff`\n"
     )
     assert out == expected_out
+
+
+def test_config_file(capsys: "CaptureFixture") -> None:
+    """Test setting in config file."""
+    file = os.path.join("tests", "invalid_data", "automagic.ipynb")
+    try:
+        with open("setup.cfg", "w") as handle:
+            handle.write("[nbqa.skip_bad_cells]\n" "black = 1\n")
+        main(["black", file, "--nbqa-diff", "--nbqa-skip-bad-cells"])
+        out, _ = capsys.readouterr()
+        expected_out = (
+            "\x1b[1mCell 1\x1b[0m\n"
+            "------\n"
+            f"--- {file}\n"
+            f"+++ {file}\n"
+            "@@ -1,2 +1,2 @@\n"
+            " if True:\n"
+            "\x1b[31m-    print('definitely valid')\n"
+            '\x1b[0m\x1b[32m+    print("definitely valid")\n'
+            "\x1b[0m\nTo apply these changes use `--nbqa-mutate` instead of `--nbqa-diff`\n"
+        )
+        assert out == expected_out
+    finally:
+        os.remove("setup.cfg")
