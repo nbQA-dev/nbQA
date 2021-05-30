aa6613df75e96819d49b1e9bd908db3d5fd2736e
Marco Edward Gorelli <marcogorelli@protonmail.com>
2021-05-30 10:39:55 +0100

    dont try processing cells with multiline magics (#603)
    * dont try processing cells with multiline magics
    
    * safer cell sep

--- tests/tools/test_pylint_works.py
+++ tests/tools/test_pylint_works.py
@@ -42,15 +42,9 @@ def test_pylint_works(capsys: "CaptureFixture") -> None:
         f'{notebook1}:cell_5:1:0: C0411: standard import "import pprint" should be placed before "import nbqa" (wrong-import-order)\n'  # noqa: E501
         f'{notebook1}:cell_5:2:0: C0411: standard import "import sys" should be placed before "import nbqa" (wrong-import-order)\n'  # noqa: E501
         "************* Module tests.data.notebook_with_indented_magics\n"  # noqa: E501
-        f'{notebook2}:cell_12:1:0: C0413: Import "import pprint" should be placed at the top of the module (wrong-import-position)\n'  # noqa: E501
-        f'{notebook2}:cell_12:2:0: C0413: Import "import sys" should be placed at the top of the module (wrong-import-position)\n'  # noqa: E501
         f"{notebook2}:cell_1:1:0: W0611: Unused randint imported from random (unused-import)\n"  # noqa: E501
         f"{notebook2}:cell_1:2:0: W0611: Unused get_ipython imported from IPython (unused-import)\n"  # noqa: E501
-        f"{notebook2}:cell_12:1:0: W0611: Unused import pprint (unused-import)\n"  # noqa: E501
-        f"{notebook2}:cell_12:2:0: W0611: Unused import sys (unused-import)\n"  # noqa: E501
         f'{notebook2}:cell_3:3:0: C0411: standard import "import operator" should be placed before "from IPython import get_ipython" (wrong-import-order)\n'  # noqa: E501
-        f'{notebook2}:cell_12:1:0: C0411: standard import "import pprint" should be placed before "from IPython import get_ipython" (wrong-import-order)\n'  # noqa: E501
-        f'{notebook2}:cell_12:2:0: C0411: standard import "import sys" should be placed before "from IPython import get_ipython" (wrong-import-order)\n'  # noqa: E501
         "\n"
         "-----------------------------------\n"
         "Your code has been rated at 4.32/10\n"
