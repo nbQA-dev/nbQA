220a3653d770ef80bcae68d3869d33db4b326328
MarcoGorelli <marcogorelli@protonmail.com>
2021-06-25 09:06:41 +0100

    py36 coverage

--- tests/test_local_script.py
+++ tests/test_local_script.py
@@ -1,6 +1,8 @@
 """Tets running local script."""
 import os
 
+import pytest
+
 from nbqa.__main__ import main
 
 
@@ -10,5 +12,26 @@ def test_local_script() -> None:
     os.chdir(os.path.join("tests", "invalid_data"))
     try:
         main(["foobarqux", "."])
+    finally:
+        os.chdir(cwd)
+
+
+def test_local_module() -> None:
+    """Test local module is picked up."""
+    cwd = os.getcwd()
+    os.chdir(os.path.join("tests", "invalid_data"))
+    try:
+        main(["mymod", "."])
+    finally:
+        os.chdir(cwd)
+
+
+def test_local_nonfound() -> None:
+    """Test local module is picked up."""
+    cwd = os.getcwd()
+    os.chdir(os.path.join("tests", "invalid_data"))
+    try:
+        with pytest.raises(ModuleNotFoundError):
+            main(["fdsfda", "."])
     finally:
         os.chdir(cwd)
