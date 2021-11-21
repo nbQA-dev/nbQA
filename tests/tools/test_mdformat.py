7fe2cdeaf7ecc15b2681acb21902e547d6f33224
Marco Gorelli <marcogorelli@protonmail.com>
2021-11-21 15:24:54 +0000

    map blacken-docs

---
+++ tests/tools/test_mdformat.py
@@ -0,0 +1,56 @@
+"""Check mdformat works."""
+
+import difflib
+import os
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from nbqa.__main__ import main
+
+if TYPE_CHECKING:
+
+    from _pytest.capture import CaptureFixture
+
+
+def test_mdformat(tmp_notebook_for_testing: Path) -> None:
+    """
+    Check pyupgrade works. Should only reformat code cells.
+
+    Parameters
+    ----------
+    tmp_notebook_for_testing
+        Temporary copy of :code:`tmp_notebook_for_testing.ipynb`.
+    """
+    # check diff
+    with open(tmp_notebook_for_testing, encoding="utf-8") as handle:
+        before = handle.readlines()
+    path = os.path.join("tests", "data", "notebook_for_testing.ipynb")
+
+    main(["mdformat", os.path.abspath(path), "--nbqa-md"])
+    with open(tmp_notebook_for_testing, encoding="utf-8") as handle:
+        after = handle.readlines()
+
+    diff = difflib.unified_diff(before, after)
+    result = "".join(i for i in diff if any([i.startswith("+ "), i.startswith("- ")]))
+    expected = (
+        '-    "First level heading\\n",\n-    "==="\n+    "# First level heading"\n'
+    )
+    assert result == expected
+
+
+def test_mdformat_works_with_empty_file(capsys: "CaptureFixture") -> None:
+    """
+    Check mdformat works with empty notebook.
+
+    Parameters
+    ----------
+    capsys
+        Pytest fixture to capture stdout and stderr.
+    """
+    path = os.path.abspath(os.path.join("tests", "data", "footer.ipynb"))
+
+    main(["mdformat", path, "--nbqa-diff", "--nbqa-md"])
+
+    out, err = capsys.readouterr()
+    assert out == ""
+    assert err == ""
