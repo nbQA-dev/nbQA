7425e4061eb8e0fb477b422d68ac94f0c3f48449
MarcoGorelli <>
2022-12-24 16:51:57 +0000

    fix one?

--- tests/tools/test_black.py
+++ tests/tools/test_black.py
@@ -259,7 +259,10 @@ def test_black_works_with_leading_comment(capsys: "CaptureFixture") -> None:
         "To apply these changes, remove the `--nbqa-diff` flag\n"
     )
     expected_err = (
-        rf"reformatted {path}\n" r"\n" r"All done! .*\n" r"1 file reformatted.\n"
+        rf"reformatted {re.escape(path)}\n"
+        r"\n"
+        r"All done! .*\n"
+        r"1 file reformatted.\n"
     )
     assert expected_out == out
     assert re.search(expected_err, err) is not None
